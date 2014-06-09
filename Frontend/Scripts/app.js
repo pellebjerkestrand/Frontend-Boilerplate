4122f616caedc994cd69e0790cf3c32c636115d7
Pelle Bjerkestrand <pelle.bjerkestrand@creuna.no>
2014-06-09 18:07:30 +0200

    moar updates

--- Frontend/Scripts/app.js
+++ Frontend/Scripts/app.js
@@ -1,3 +1,4 @@
+// @codekit-append "Vendor/loglevel.js"
+// @codekit-append "Modules/behaviors.js"
 // @codekit-append "Modules/indicate.javascript.js"
 // @codekit-append "Modules/initialization.js"
-// @codekit-append "Modules/behaviors.js"
